6396346a607216f35cd17e14bdcbf126812b4291
John Lynch <johnf.lynch@yahoo.ie>
2024-07-22 18:29:26 +0000

    add recipe and cloudinary function

--- main/settings.py
+++ main/settings.py
@@ -48,15 +48,31 @@ INSTALLED_APPS = [
     
     #Apps
     'home',
+    'recipes',
 
     #Others
     'crispy_forms',
     'crispy_bootstrap5',
-
+    'cloudinary',
+    'cloudinary_storage',
+    'djrichtextfield',
 
 ]
 
 SITE_ID = 1
+
+DJRICHTEXTFIELD_CONFIG = {
+    'js': ['//cdn.ckeditor.com/4.14.0/standard/ckeditor.js'],
+    'init_template': 'djrichtextfield/init/ckeditor.js',
+    'settings': {
+        'toolbar': [
+            ['Format', 'Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList'], ['Undo', 'Redo'],
+            ['Maximize']
+        ],
+        'format_tags': 'p;h1;h2;h3'
+    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -170,6 +186,10 @@ LOGIN_REDIRECT_URL = '/'
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+# Cloudinary Settings
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

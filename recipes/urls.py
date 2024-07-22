aff37f061ef55eee5544469cf3ff93b9652a1a13
John Lynch <johnf.lynch@yahoo.ie>
2024-07-22 22:31:30 +0000

    fgfgg

---
+++ recipes/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import (
+    AddRecipe, Recipes,
+    
+)
+
+urlpatterns = [
+    path("add/", AddRecipe.as_view(), name="add_recipe"),
+    path("", Recipes.as_view(), name="recipes"),
+]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+# Checks that the user is logged in first
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Recipe
+from .forms import RecipeForm
+
+# LoginRequiredMixin - Checks that the user is logged in first
+class AddRecipe(LoginRequiredMixin, CreateView):
+    """Add recipe view"""
+
+    template_name = "recipes/add_recipe.html"
+    model = Recipe
+    form_class = RecipeForm
+    success_url = "/recipes/"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(AddRecipe, self).form_valid(form)
